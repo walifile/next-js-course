7c08e4f97e32835953be0ec2db2addce2c3255e0
wali ahmad <waliahmadfiles@gmail.com>
2023-12-26 01:16:39 +0500

    add routing

---
+++ app/product/[id]/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Detail = ({ params }) => {
+  return <div>Detail page {params.id}</div>;
+};
+
+export default Detail;
---
+++ app/product/item/[name]/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Detail = ({ params }) => {
+  return <div>Item Detail page {params.name} </div>;
+};
+
+export default Detail;
