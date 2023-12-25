06621d7d6f45e93546e6eb8b89b9da0655ff1bff
wali ahmad <waliahmadfiles@gmail.com>
2023-12-26 02:47:25 +0500

    add 404 pages

---
+++ app/product/[...not-found]/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFound = () => {
+  return <div>This product Page is not available</div>;
+};
+
+export default NotFound;
---
+++ app/product/error.js
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const Error = () => {
+  return <div>Try Again Lator</div>;
+};
+
+export default Error;
